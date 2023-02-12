b68f5c70496a6bd9bd834d4a7bea3528bc884d8a
Maxim Khrypunov <khrypunovmax@gmail>
2023-02-12 11:52:09 +0200

    HW_#25

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { weatherConfig } from "./service/weather-config.js";
 const dataProcessor = new DataProcessor(weatherConfig.url);
 async function displayTemperatures() 
 {
-const data = await dataProcessor.createWeatherObject("Jerusalem","2023-02-11","2023-02-14",11,23)
+const data = await dataProcessor.createWeatherObject("Jerusalem","2023-02-12","2023-02-12",20,23)
 }
 displayTemperatures()
 
--- src/service/DataProcessor.js
+++ src/service/DataProcessor.js
@@ -2,14 +2,18 @@ import { weatherConfig } from "./weather-config.js";
 
 export class DataProcessor {
     #url;
+
     constructor(url) {
         this.#url = url;
     }
-    async getData(latitude, longitude) {
+
+    async getData(latitude, longitude)
+    {
         const responseFromServes =
             await fetch(`${this.#url}&latitude=${latitude}&longitude=${longitude}`);
         return responseFromServes.json();
     }
+
     async getTemperatureData(city, startDate, endDate) {
         const latitudeOfCity = weatherConfig.cities[city].latitude;
         const longitudeofCity = weatherConfig.cities[city].longitude;
@@ -18,36 +22,55 @@ export class DataProcessor {
 &end_date=${endDate}`);
         return responseFromServes.json();
     }
+
     async createWeatherObject(city, startDate, endDate, hourFrom, hourTo) {
-        const control = this.dataСhecking(startDate, endDate, hourFrom, hourTo);
-        if (control)
-        {const dataFromServer = await this.getTemperatureData(city, startDate, endDate);
-        const temperatures = dataFromServer.hourly.temperature_2m;
-        const times = dataFromServer.hourly.time;
-        const weatherObject = [];
-        times.forEach((element, index) => {
-            const time = new Date(element).getHours();
-            if (time >= hourFrom && time <= hourTo) {
-                const dateForArray = element.split("T")[0];
-                weatherObject.push({ date: dateForArray, hour: time, temperature: temperatures[index] })
-            }
-        });
-        return console.log(weatherObject);}
+        const controlRes = this.dataСhecking(startDate, endDate, hourFrom, hourTo);
+        if (controlRes) {
+            const dataFromServer = await this.getTemperatureData(city, startDate, endDate);
+            if(dataFromServer.error) {return console.log(dataFromServer);}
+            const temperatures = dataFromServer.hourly.temperature_2m;
+            const times = dataFromServer.hourly.time;
+            const weatherObject = [];
+            times.forEach((element, index) => {
+                const time = new Date(element).getHours();
+                if (time >= hourFrom && time <= hourTo) {
+                    const dateForArray = element.split("T")[0];
+                    weatherObject.push({ date: dateForArray, hour: time, temperature: temperatures[index] })
+                }
+            });
+            return console.log(weatherObject)};
+        
     }
-        dataСhecking(startDate,endDate,hourFrom,hourTo) {
-        const  currentDate = new Date();
-        const controlResult = true;
-        if (hourFrom < 0 || hourFrom > 23) {return console.log("Error.Start time value can be set from 0 to 23"); controlResult = false}
-        else if (hourTo < 0 || hourTo > 23) {return  console.log("Error. End time value can be set from 0 to 23"); controlResult = false}
-        else if (currentDate > startDate) { return console.log("The start date that you specified is less than the current date"); controlResult = false}
-        else if (this.maximumDurationOfForecast()<endDate)  {return console.log("Maximum weather forecast duration 16 days");controlResult = false}
-        return controlResult;
+    dataСhecking(startDate, endDate, hourFrom, hourTo) 
+    {
+        let controlResult = true;
+        const controlStartDay = this.checkingStartDay(startDate)
+        if (controlStartDay)
+        {
+        if (hourFrom < 0 || hourFrom > 23) { console.log("Error.Start time value can be set from 0 to 23"); return controlResult = false; }
+        else if (hourTo < 0 || hourTo > 23) {console.log("Error. End time value can be set from 0 to 23"); return controlResult = false;}
+        else if (this.maximumDurationOfForecast() < endDate) 
+        {console.log("Maximum weather forecast duration 16 days"); return controlResult = false; }
+        return controlResult;}
     }
+
     maximumDurationOfForecast() 
     {
         const controlStartDate = new Date();
         const maximumDuration = controlStartDate.setDate(controlStartDate.getDate() + 16);
         return maximumDuration;
     }
+
+    checkingStartDay(startDate) 
+    {
+        let controlResult = true;
+        const now = new Date();
+        const currentDate = new Date(now.getFullYear(), now.getMonth(), now.getDate()).valueOf();
+        const other = new Date(startDate);
+        const dataDate = new Date(other.getFullYear(), other.getMonth(), other.getDate()).valueOf();
+        if (currentDate > dataDate) { return console.log("Error! Invalid start date. Start date is less than current date"),controlResult = false; }
+        return controlResult;
+    }
+
 }
 
