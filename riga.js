4582a306ee88b6205f3de33b2cef6e8d9de42c78
Maxim Khrypunov <khrypunovmax@gmail>
2023-02-22 15:23:36 +0200

    HW#28

--- riga.js
+++ riga.js
@@ -1,17 +1,27 @@
-const promise1 = Promise.resolve('First')
-const promise2 = Promise.resolve('Second')
-const promise3 = Promise.reject('Third')
-const promise4 = Promise.resolve('Fourth')
-
-const runPromises = async () => {
-	const res1 = await Promise.all([promise1, promise2])
-	const res2  = await Promise.all([promise3, promise4])
-	return [res1, res2]
-}
-
-runPromises()
-	.then(res => console.log(res))
-	.catch(err => console.log(err))
-
+class Deferred 
+{
+    constructor() {
+      this.callbacks = [];
+      this.resolvedValue = undefined;
+    }
   
+    then(callback) {
+      this.callbacks.push(callback);
+      return this;
+    }
+  
+    resolve(value) {
+      this.resolvedValue = value;
+      let result = this.resolvedValue;
+      for (let i = 0; i < this.callbacks.length; i++) {
+        result = this.callbacks[i](result);
+      }
+    }
+  }
 
+const d = new Deferred()
+d.then(function(res){ console.log("1 ", res); return "a"; });
+d.then(function(res){ console.log("2 ", res); return "b"; });
+d.then(function(res){ console.log("3 ", res);
+return "c"; });
+d.resolve('hello');
